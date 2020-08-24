fba84366bc1fed9e118ca8f1d51d7564bfb51e17
Naoshi Arimori <naoshiarimori@gmail.com>
2020-08-24 20:21:04 -0300

    Initial commit

---
+++ src/routes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+routes.post('/sessions', SessionController.create);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+module.exports = routes;
+
+// Tipos de parametros:
+// Query: commom parameters /users?name='Naoshi' request.query
+// route params: mostly used to identify resources /users/1 request.params
+// Request body: using json request.body
